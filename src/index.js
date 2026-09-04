48c6eb585bfde08c61cb32b167019a24418ad4d2
Shivam Pandey <shivam221pandey@gmail.com>
2026-09-04 22:24:28 +0530

    DB add

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/*
+import mongoose  from "mongoose";
+import { DB_NAME } from "./constants"; */
+import dns from 'dns'
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+const app = express();
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on('error', (error) => {
+            console.log("ERR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=> {
+            console.log(`App is listining on port ${process.env.PORT}`)
+        })
+    }catch (error) {
+        console.log("ERROR", error)
+        throw err
+    }
+})
+    */
