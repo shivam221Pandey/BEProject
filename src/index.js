38b845508403c2cd130ee77fdb7dcd6e7dce5562
Shivam Pandey <shivam221pandey@gmail.com>
2026-09-05 09:28:39 +0530

    add cookie-parser and cors middleware; implement ApiError and ApiResponse classes; add asyncHandler utilityadd cookie-parser and cors middleware; implement ApiError and ApiResponse classes; add asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { DB_NAME } from "./constants"; */
 import dns from 'dns'
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import app from './app.js'
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 dotenv.config({
@@ -11,6 +12,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Sserver is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log('MONGO bd connection failed !!!', err)
+})
 
 
 
